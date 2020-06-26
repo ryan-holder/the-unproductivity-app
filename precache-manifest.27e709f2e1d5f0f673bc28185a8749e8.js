b5dbc91d67721f1d3518b83328a215e6ed630c48
Ryan <rjhold92@hotmail.co.nz>
2020-06-26 16:50:02 +1200

    Updates

---
+++ precache-manifest.27e709f2e1d5f0f673bc28185a8749e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15bf83bef3d32e60914ac444c5ff0c8",
+    "url": "/the-unproductivity-app/index.html"
+  },
+  {
+    "revision": "d2d8c1ce553bc1511cb7",
+    "url": "/the-unproductivity-app/static/css/main.e2268719.chunk.css"
+  },
+  {
+    "revision": "8f4d8002ac530659f906",
+    "url": "/the-unproductivity-app/static/js/2.fbea1037.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-unproductivity-app/static/js/2.fbea1037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d8c1ce553bc1511cb7",
+    "url": "/the-unproductivity-app/static/js/main.3a1917d3.chunk.js"
+  },
+  {
+    "revision": "bcc118c7e27090bd1163",
+    "url": "/the-unproductivity-app/static/js/runtime-main.3b179778.js"
+  }
+]);
